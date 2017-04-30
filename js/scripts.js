eee76411fa02a5e383da37b522c16aec03c19837
eileen <eileen@eileen.com>
2017-04-30 20:26:29 +0200

    adding isNaN

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,10 @@ var a = prompt('Podaj długość podstawy trójkąta:'),
 	h = prompt('Podaj wysokość trójkąta:'),
 	triangleArea = a*h/2;
 
-console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+if (isNaN(triangleArea)) {
+	console.log('Nieprawidlowa wartosc');
+	alert('Nieprawidlowa wartosc');
+} else {
+	console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+	alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+}
